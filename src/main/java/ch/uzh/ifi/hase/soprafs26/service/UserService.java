bdb1347c49b94e5443b050d54fb420d39b6da73c
sjfess <samueljonas.fessler@uzh.ch>
2026-05-07 14:09:12 +0200

    Added tests for ChatService, GameCardHelper

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/UserService.java
@@ -1,7 +1,6 @@
 package ch.uzh.ifi.hase.soprafs26.service;
 
 import ch.uzh.ifi.hase.soprafs26.constant.UserStatus;
-import ch.uzh.ifi.hase.soprafs26.entity.FriendRequest;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
 import org.slf4j.Logger;
